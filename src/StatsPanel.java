fe68569a7da732f85ec0b61bff1d5cc62e9d01a1
radunsb <93169946+radunsb@users.noreply.github.com>
2025-02-20 12:00:48 -0500

    Refactor StatsPanel

--- src/StatsPanel.java
+++ src/StatsPanel.java
@@ -18,69 +18,80 @@ public class StatsPanel extends JPanel {
     private ArrayList<JLabel> resultsLabels;
 
     public StatsPanel(JPanel cardsPanel) {
-
         this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-
-        JLabel title = new JLabel("Your Stats");
-        this.add(title);
-        title.setAlignmentX(Component.CENTER_ALIGNMENT);
-
-        JLabel subtitle = new JLabel("(Past 30 Days)");
-        this.add(subtitle);
-        subtitle.setAlignmentX(Component.CENTER_ALIGNMENT);
-
-        this.add(Box.createRigidArea(new Dimension(0,40)));
-
-        resultsPanel = new JPanel();
-        resultsLabels = new ArrayList<>();
-        resultsPanel.setLayout(new GridLayout(0, 2));
-        resultsPanel.add(new JLabel("Guesses"));
-        resultsPanel.add(new JLabel("Games"));
-        for(int binIndex=0; binIndex<BIN_EDGES.length; binIndex++){
-            String binName;
-            if(binIndex == BIN_EDGES.length-1){
-                // last bin
-                binName = BIN_EDGES[binIndex] + " or more";
-            }
-            else{
-                int upperBound = BIN_EDGES[binIndex+1] - 1;
-                if(upperBound > BIN_EDGES[binIndex]){
-                    binName = BIN_EDGES[binIndex] + "-" + upperBound;
-                }
-                else{
-                    binName = Integer.toString(BIN_EDGES[binIndex]);
-                }
-            }
-
-            resultsPanel.add(new JLabel(binName));
-            JLabel result = new JLabel("--");
-            resultsLabels.add(result);
-            resultsPanel.add(result);
+        for(JLabel label : constructLabels()){
+            this.add(label);
         }
-
-        resultsPanel.setMinimumSize(new Dimension(120, 120));
+        this.add(Box.createRigidArea(new Dimension(0,40)));
+        resultsLabels = new ArrayList<>();
+        resultsPanel = setupResultsPanel();
         this.add(resultsPanel);
-        resultsPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
         updateResultsPanel();
-
         this.add(Box.createVerticalGlue());
+        this.add(constructQuitButton(cardsPanel));
+        this.add(Box.createRigidArea(new Dimension(0,20)));
+        this.addComponentListener(new java.awt.event.ComponentAdapter() {
+            public void componentShown(java.awt.event.ComponentEvent e) {
+                updateResultsPanel();
+            }
+        });
+    }
 
+    private JButton constructQuitButton(JPanel cardsPanel){
         JButton quit = new JButton("Back to Home");
         quit.addActionListener(e -> {
             // See itemStateChanged in https://docs.oracle.com/javase/tutorial/uiswing/examples/layout/CardLayoutDemoProject/src/layout/CardLayoutDemo.java
             CardLayout cardLayout = (CardLayout) cardsPanel.getLayout();
             cardLayout.show(cardsPanel, ScreenID.HOME.name());
         });
-        this.add(quit);
         quit.setAlignmentX(Component.CENTER_ALIGNMENT);
+        return quit;
+    }
+    private ArrayList<JLabel> constructLabels(){
+        ArrayList<JLabel> labels = new ArrayList<>();
+        JLabel title = new JLabel("Your Stats");
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+        labels.add(title);
+        JLabel subtitle = new JLabel("(Past 30 Days)");
+        subtitle.setAlignmentX(Component.CENTER_ALIGNMENT);
+        labels.add(subtitle);
+        return labels;
+    }
 
-        this.add(Box.createRigidArea(new Dimension(0,20)));
-
-        this.addComponentListener(new java.awt.event.ComponentAdapter() {
-            public void componentShown(java.awt.event.ComponentEvent e) {
-                updateResultsPanel();
+    private ArrayList<String> constructBinNames(){
+        ArrayList<String> names = new ArrayList<>();
+        for(int binIndex=0; binIndex<BIN_EDGES.length; binIndex++) {
+            String binName;
+            if (binIndex == BIN_EDGES.length - 1) {
+                // last bin
+                binName = BIN_EDGES[binIndex] + " or more";
+            } else {
+                int upperBound = BIN_EDGES[binIndex + 1] - 1;
+                if (upperBound > BIN_EDGES[binIndex]) {
+                    binName = BIN_EDGES[binIndex] + "-" + upperBound;
+                } else {
+                    binName = Integer.toString(BIN_EDGES[binIndex]);
+                }
             }
-        });
+            names.add(binName);
+        }
+        return names;
+    }
+
+    public JPanel setupResultsPanel(){
+        JPanel setupPanel = new JPanel();
+        setupPanel.setLayout(new GridLayout(0, 2));
+        setupPanel.add(new JLabel("Guesses"));
+        setupPanel.add(new JLabel("Games"));
+        setupPanel.setMinimumSize(new Dimension(120, 120));
+        for(String binName : constructBinNames()){
+            setupPanel.add(new JLabel(binName));
+            JLabel result = new JLabel("--");
+            resultsLabels.add(result);
+            setupPanel.add(result);
+        }
+        setupPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        return setupPanel;
     }
 
 
@@ -92,13 +103,10 @@ public class StatsPanel extends JPanel {
 
     private void updateResultsPanel(){
         clearResults();
-
         GameStats stats = new StatsFile();
-
         for(int binIndex=0; binIndex<BIN_EDGES.length; binIndex++){
             final int lowerBound = BIN_EDGES[binIndex];
             int numGames = 0;
-
             if(binIndex == BIN_EDGES.length-1){
                 // last bin
                 // Sum all the results from lowerBound on up
@@ -112,7 +120,6 @@ public class StatsPanel extends JPanel {
                     numGames += stats.numGames(numGuesses);
                 }
             }
-
             JLabel resultLabel = resultsLabels.get(binIndex);
             resultLabel.setText(Integer.toString(numGames));
         }
